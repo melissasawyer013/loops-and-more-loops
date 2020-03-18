8df20d22b21ad200d6b4513f6306ad2681377725
melissasawyer013 <58709646+melissasawyer013@users.noreply.github.com>
2020-03-17 20:43:25 -0400

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,36 @@
+// for (let i = 0; i <= 10; i++) {
+//    console.log("The number is ", i); 
+// }
+
+
+// for (let i = 5; i >=5 && i <= 15; i++) {
+//     if ( i % 3 === 0) {
+//         console.log (i);
+//     }
+// }
+
+// for (let i = 40; i <=60; i+=2) {
+//     let message = `${i} little monkeys sitting in a tree
+//     `;
+//     console.log(message);
+// }
+
+// for (let i = 100; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// let love = "Boston CodeSquad";
+// console.log(love.length);
+// for (let i = 8; i < love.length; i++) {
+//     console.log("I love JavaScript");
+// }
+
+for (let i = 0; i <= 25; i++) {
+    if (i % 2 === 0) {
+        alert(`${i} is even`);
+        console.log(`${i} is even`);
+    } else {
+        alert(`${i} is odd`);
+        console.log(`${i} is odd`);
+    }
+}
